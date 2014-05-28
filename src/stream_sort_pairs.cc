9a9eadf15c7dbd9c63189fae317782a8d99e59eb
Peter Sudmant <psudmant@gmail.com>
2014-05-27 22:53:25 -0700

    first commit c-code

---
+++ src/stream_sort_pairs.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include "stream_sort_pairs.h"
+
+using namespace std;
+
+int main(){
+    cout << "up and running!\n";    
+    return 0;
+}
